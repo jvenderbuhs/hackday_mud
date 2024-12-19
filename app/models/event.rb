ff315364e1592b48152775b9c4a6c4efac3e0ede
Jordan venderBuhs <jordan.venderbuhs@iheartjane.com>
2024-12-19 16:42:09 -0600

    Added very basic deathmatch combat

--- app/models/event.rb
+++ app/models/event.rb
@@ -37,7 +37,7 @@ class Event < ApplicationRecord
     'Sword' => {
       type: :weapon,
       bonus: 0,
-      damage: '1d4'
+      damage: '1d6'
     },
     'Shield' => {
       type: :shield,
@@ -74,6 +74,8 @@ class Event < ApplicationRecord
         wield(character, message)
       when 'wear'
         wear(character, message)
+      when 'fight'
+        fight(character, message)
       when 'pickup'
         pickup(character, message)
       when 'move'
@@ -233,6 +235,82 @@ class Event < ApplicationRecord
           character: character,
           room: character.current_room,
           info: "You were not carrying a '#{ item }'.",
+          personal: true
+        )
+      end
+    end
+
+    def fight(character, enemy)
+      enemy = enemy.titleize
+      enemies_available = ROOMS[character.current_room][:enemies]
+      if enemies_available.include?(enemy)
+        combatant = ENEMIES[enemy].dup
+        score = JSON.parse(character.score)
+        if score["weapon"].blank?
+          Event.create(
+            character: character,
+            room: character.current_room,
+            info: "You don't have a weapon, why are you picking a fight?",
+            personal: true
+          )
+        end
+
+        max_hp = score["hp"]
+        weapon = ITEMS[score["weapon"]]
+        to_hit = score["dex"] + weapon[:bonus]
+        char_damage_rolls, char_damage_die = ITEMS[score["weapon"]][:damage].split("d").map(&:to_i)
+
+        armor_class = 10 + score["dex"]
+        armor_class += ITEMS[score["shield"]][:bonus] if score["shield"].present?
+        armor_class += ITEMS[score["armor"]][:bonus] if score["armor"].present?
+
+        enemy_damage_rolls, enemy_damage_die = ITEMS[score["weapon"]][:damage].split("d").map(&:to_i)
+
+        player_won = false
+        while (score["hp"] > 0 && combatant[:hit_points] > 0) do
+          if rand(20) + to_hit >= combatant[:armor_class]
+            char_damage_rolls.times do
+              combatant[:hit_points] -= rand(char_damage_die) + score["str"]
+            end
+          end
+          if combatant[:hit_points] <= 0
+            player_won = true
+            break
+          end
+
+          if rand(20) + combatant[:accuracy] >= armor_class
+            enemy_damage_rolls.times do
+              score["hp"] -= rand(enemy_damage_die)
+            end
+          end
+        end
+        if player_won
+          damage_taken = max_hp - score["hp"]
+          score["hp"] = max_hp
+          score["xp"] += combatant[:xp]
+          character.update(score: score.to_json)
+          Event.create(
+            character: character,
+            room: character.current_room,
+            info: "You defeated '#{ enemy }' after taking #{ damage_taken } damage and received #{ combatant[:xp] } xp as a reward. You rest after combat recovering all your health.",
+            personal: true
+          )
+        else
+          score["hp"] = max_hp
+          score["xp"] = 0
+          character.update(score: score.to_json)
+          Event.create(
+            character: character,
+            room: character.current_room,
+            info: "You were defeated by '#{ enemy }' have lost all your xp from this level.",
+            personal: true
+          )
+        end
+      else
+        Event.create(
+          character: character,
+          room: character.current_room,
+          info: "You could not find a '#{ enemy }' here to fight.",
           personal: true
         )
       end
